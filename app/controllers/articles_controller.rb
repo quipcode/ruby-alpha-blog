a77cc00a9d2c448b9c432018a1f5b371cda574aa
quipcode <abdulkarim8ahmed@gmail.com>
2022-11-15 21:56:37 -0800

    created new article form and create functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,23 @@
+
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.find(params[:id])
-    end
-    def index
-        @articles = Article.all
-    end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # https://stackoverflow.com/questions/1997201/what-exactly-does-save-save-do
+    @article.save!
+    redirect_to @article
+  end
+
 end
